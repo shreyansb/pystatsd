07d9089454fe6097975332f51fa33bc624e6a576
Shreyans Bhansali <shreyans@venmo.com>
2012-04-21 13:07:13 -0400

    statsd modified for venmo's use

--- statsd/__init__.py
+++ statsd/__init__.py
@@ -1,35 +1,15 @@
-import os
-import socket
-
-try:
-    from django.conf import settings
-except ImportError:
-    settings = None
+import venmo_constants
 
 from client import StatsClient
-
 
 __all__ = ['StatsClient', 'statsd']
 
 VERSION = (0, 5, 0)
 __version__ = '.'.join(map(str, VERSION))
 
-
 statsd = None
 
-if settings:
-    try:
-        host = getattr(settings, 'STATSD_HOST', 'localhost')
-        port = getattr(settings, 'STATSD_PORT', 8125)
-        prefix = getattr(settings, 'STATSD_PREFIX', None)
-        statsd = StatsClient(host, port, prefix)
-    except (socket.error, socket.gaierror, ImportError):
-        pass
-elif 'STATSD_HOST' in os.environ:
-    try:
-        host = os.environ['STATSD_HOST']
-        port = int(os.environ['STATSD_PORT'])
-        prefix = os.environ.get('STATSD_PREFIX')
-        statsd = StatsClient(host, port, prefix)
-    except (socket.error, socket.gaierror, KeyError):
-        pass
+host = venmo_constants.STATSD_HOST
+port = venmo_constants.STATSD_PORT
+prefix = getattr(venmo_constants, 'STATSD_PREFIX', None)
+statsd = StatsClient(host, port, prefix)
