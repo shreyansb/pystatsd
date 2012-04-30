8fd33f87183024d660e1da36261be672d08fed3b
Shreyans Bhansali <shreyans@venmo.com>
2012-04-29 21:41:07 -0400

    added a new decorator/context manager for logging an attempt on and error in a function

--- statsd/__init__.py
+++ statsd/__init__.py
@@ -1,4 +1,15 @@
+"""
+Originally written by James Socol
+https://github.com/jsocol/pystatsd
+
+Modified by Shreyans Bhansali at Venmo
+21 April 2012
+"""
+import sys; import os; APPROOT = os.environ.get('APPROOT') or '/ebs/appvenmo'; sys.path.append(APPROOT);
+
 import venmo_constants
+
+from lib.utils import get_stage
 
 from client import StatsClient
 
@@ -10,6 +21,6 @@ __version__ = '.'.join(map(str, VERSION))
 statsd = None
 
 host = venmo_constants.STATSD_HOST
-port = venmo_constants.STATSD_PORT
-prefix = getattr(venmo_constants, 'STATSD_PREFIX', None)
+port = getattr(venmo_constants, 'STATSD_PORT', 8125)
+prefix = get_stage(APPROOT)
 statsd = StatsClient(host, port, prefix)
--- statsd/client.py
+++ statsd/client.py
@@ -1,8 +1,15 @@
+"""
+Originall written by James Socol
+https://github.com/jsocol/pystatsd
+
+Modified by Shreyans Bhansali at Venmo
+21 April 2012
+"""
+
 from functools import wraps
 import random
 import socket
 import time
-
 
 class _Timer(object):
     """A context manager/decorator for statsd.timing()."""
@@ -29,6 +36,32 @@ class _Timer(object):
         self.ms = int(round(1000 * dt))  # Convert to ms.
         self.client.timing(self.stat, self.ms, self.rate)
 
+
+class _LogAttemptAndError(object):
+    """A context manager/decorator that logs an attempt made to 
+    run a function, and any error raised during the function's
+    execution, used by stats.log_attempt_and_error().
+    """
+
+    def __init__(self, client, base_stat):
+        self.client = client
+        self.base_stat = base_stat
+
+    def __call__(self, f):
+        @wraps(f)
+        def wrapper(*args, **kw):
+            with self:
+                return f(*args, **kw)
+        return wrapper
+
+    def __enter__(self):
+        attempt_stat = "%s.attempt" % self.base_stat
+        self.client.incr(attempt_stat)
+
+    def __exit__(self, type, value, traceback):
+        if isinstance(value, Exception):
+            error_stat = "%s.error" % self.base_stat
+            self.client.incr(error_stat)
 
 class StatsClient(object):
     """A client for statsd."""
@@ -58,6 +91,9 @@ class StatsClient(object):
     def gauge(self, stat, value, rate=1):
         """Set a gauge value."""
         self._send(stat, '%d|g' % value, rate)
+
+    def log_attempt_and_error(self, base_stat):
+        return _LogAttemptAndError(self, base_stat)
 
     def _send(self, stat, value, rate=1):
         """Send data to statsd."""
